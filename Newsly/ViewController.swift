ce6d43a757191d1231140bdef8bde7c7efc8ad95
0ofKim <pos00042@naver.com>
2025-07-19 17:35:08 +0900

    Create Project

---
+++ Newsly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Newsly
+//
+//  Created by 0ofKim on 7/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
